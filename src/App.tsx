384efaac6b09b2a99559c9e094403159168758df
baptiste geffroy <baptiste@MacBook-Pro-de-baptiste.local>
2022-01-27 17:47:35 +0100

    create App component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+import React, { FunctionComponent } from 'react';
+  
+// const App: React.FC = () => {
+const App: FunctionComponent = () => {
+ const name: String = 'React';
+    
+ return (
+  <h1>Hello, {name} !</h1>
+ )
+}
+  
+export default App;
